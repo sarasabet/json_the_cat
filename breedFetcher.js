f046149df7ba912448a63130385c1c4dffe65706
Sara Sabet <sara.ilbeigi@gmail.com>
2021-08-26 22:30:00 +0000

    refactor/setup test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,21 @@
-
 const request = require('request');
-const breeds = process.argv.slice(2).join('').slice(0,3)
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breeds}`;
 
-const getCatByBreed = (breed) =>{
-  if(breed === undefined || (!breed)) {
-    console.log('Breed does not found ')
-  } else {
-    console.log('Breed description', breed)
-  }
- 
-}
-const breedFetcher = (callback, breeds) => {
-  request(url, (error, response, body) => {
+const url = "https://api.thecatapi.com/v1/breeds/search?q=";
 
+const fetchBreedDescription = function(breedName, callback) {
+
+  request((url + breedName), (error, response, body) => {
     if (error) {
-      console.log('Error ocuured:', error)
-    }
-    const data = JSON.parse(body)
-    const breed = data[0].description
-    if (breed === undefined) {
-      console.log('Breed does not exits ')
-    }
-    callback(breed)
+      callback(error, null);
+    } 
+      const data = JSON.parse(body);
+      const breed = data[0];
+      if (breed) {
+        callback(null, breed.description);
+      } else {
+        callback("breed not found")
+      }
   });
 };
 
-breedFetcher(getCatByBreed, breeds)
+module.exports = { fetchBreedDescription };
