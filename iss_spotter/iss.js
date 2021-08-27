13bd8bcc642b36e196aa24a75a819771f7bd5c96
Sara Sabet <sara.ilbeigi@gmail.com>
2021-08-27 00:02:55 +0000

    Fetch Flyover Times for ISS

--- iss_spotter/iss.js
+++ iss_spotter/iss.js
@@ -24,23 +24,45 @@ const request = require('request');
 // };
 
 // module.exports = { fetchMyIP };
-const fetchCoordsByIP = function(ip, callback) {
-  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+// const fetchCoordsByIP = function(ip, callback) {
+//   request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+//     if (error) {
+//       callback(error, null);
+//       return;
+//     }
+
+//     if (response.statusCode !== 200) {
+//       callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+//       return;
+//     }
+
+//     const { latitude, longitude } = JSON.parse(body);
+
+//     callback(null, { latitude, longitude });
+//   });
+// };
+
+// // Don't need to export the other function since we are not testing it right now.
+// module.exports = { fetchCoordsByIP };
+// 
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
     }
 
     if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
       return;
     }
 
-    const { latitude, longitude } = JSON.parse(body);
-
-    callback(null, { latitude, longitude });
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
   });
 };
 
-// Don't need to export the other function since we are not testing it right now.
-module.exports = { fetchCoordsByIP };
+// Don't need to export the other functions since we are not testing them right now.
+module.exports = { fetchISSFlyOverTimes };
